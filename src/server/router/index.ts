1601477c872876c30138597ff71f57bf40d57c1d
Vikrom Narula <vikrom.nar@gmail.com>
2022-08-20 08:26:33 +0700

    Fix code form examples prisma to stations

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,11 +2,11 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
+import { stationRouter } from "./station";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter);
+  .merge("station.", stationRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
